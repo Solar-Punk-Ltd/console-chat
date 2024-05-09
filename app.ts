c106b89153daeff88afee9d3887f6b4f8ff4d05a
Peter Ott <ottpeter@tuta.io>
2024-05-09 17:06:21 +0200

    Added dotenv

--- app.ts
+++ app.ts
@@ -1,1 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { updateUserList } from "./libs/chat";
+
 console.log("Hello World!");
+console.log(process.env.HELLO)
---
+++ dist/app.cjs
@@ -0,0 +1,8 @@
+"use strict";
+//import dotenv from 'dotenv';
+//dotenv.config();
+Object.defineProperty(exports, "__esModule", { value: true });
+const chat_1 = require("libs/chat");
+console.log("Hello World!");
+(0, chat_1.updateUserList)("f");
+//console.log(process.env.HELLO)
